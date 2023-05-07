43cb532edfa1d61a6ce4b9a2926b4d8d8fcaa63f
Andre Julius <noromoron@gmail.com>
2023-05-08 00:50:15 +0200

    Add new binary only for registering commands
    I am doing this in order to get rid of the messsage content intent requirement.
    After that I can just run this binary and it should update the commands.

---
+++ src/bin/register_commands.rs
@@ -0,0 +1,31 @@
+use discord_banner_bot::{commands::commands, error::Error, utils::start_logging, Settings};
+use poise::serenity_prelude;
+use tracing::{error, info};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<(), Error> {
+    Settings::init()?;
+    let settings = Settings::get();
+    println!("Using log level: {}", settings.bot.log_level);
+
+    start_logging(&settings.bot.log_level);
+
+    let http = serenity_prelude::HttpBuilder::new(&settings.bot.token).build();
+    let commands = poise::builtins::create_application_commands(&commands());
+
+    match serenity_prelude::Command::set_global_application_commands(http, |a| {
+        *a = commands;
+        a
+    })
+    .await
+    {
+        Ok(set_cmds) => {
+            info!("{} commands have been set globally", set_cmds.len());
+        }
+        Err(err) => {
+            error!("{err}");
+        }
+    }
+
+    Ok(())
+}
