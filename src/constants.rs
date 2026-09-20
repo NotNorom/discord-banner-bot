c422b7928bab5c66f2db453cfbedb6ebb0a5ddef
Andre Julius <noromoron@gmail.com>
2026-09-20 19:28:41 +0200

    Revert back to 10mb limit. Apparently the upload limit was only increased for user uploads...

--- src/constants.rs
+++ src/constants.rs
@@ -8,7 +8,7 @@ use poise::serenity_prelude;
 pub const USER_AGENT: &str = concat!(env!("CARGO_PKG_REPOSITORY"), " - ", env!("CARGO_PKG_VERSION"));
 
 /// Maximum image size in bytes for uploads to discord
-pub const MAXIMUM_IMAGE_SIZE: usize = 1024 * 1024 * 20; // 20mb
+pub const MAXIMUM_IMAGE_SIZE: usize = 1024 * 1024 * 10; // 10mb
 
 /// Maximum message length for discord
 pub const DISCORD_MESSAGE_CONTENT_LIMIT: usize = serenity_prelude::constants::MESSAGE_CODE_LIMIT;
